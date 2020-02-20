6c31eff8431528f213317fb3eec70c57ae6d8fbb
Hermann W <azfalt@gmail.com>
2020-02-20 12:02:30 +0100

    Storage saves data as a single dictionary instead of separate values

--- Solario/Data/RawDataStorage/RawDataStorage.swift
+++ Solario/Data/RawDataStorage/RawDataStorage.swift
@@ -12,15 +12,38 @@ class RawDataStorage: RawDataStorageProtocol {
 
     private let defaults = UserDefaults.standard
 
+    private let key = "storage"
+
+    private var texts = [String: String]()
+
+    init() {
+        tryLoadFromDefaults()
+    }
+
+    // MARK: - RawDataStorageProtocol
+
     func rawDataFile(url: URL) -> RawDataFile? {
-        if let text = defaults.string(forKey: url.absoluteString) {
+        if let text = texts[url.absoluteString] {
             return RawDataFile(text: text)
         }
         return nil
     }
 
     func setRawDataFile(_ file: RawDataFile, url: URL) {
-        defaults.set(file.text, forKey: url.absoluteString)
+        texts[url.absoluteString] = file.text
+        saveToDefaults()
+    }
+
+    // MARK: -
+
+    private func tryLoadFromDefaults() {
+        if let texts = defaults.object(forKey: key) as? [String: String] {
+            self.texts = texts
+        }
+    }
+
+    private func saveToDefaults() {
+        defaults.set(texts, forKey: key)
         defaults.synchronize()
     }
 }
